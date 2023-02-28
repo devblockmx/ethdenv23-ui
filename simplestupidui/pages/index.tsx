5f348b3a3261aeb0de1f98f5be7401c8b29412cb
Greg Schwartz <greg@gregschwartz.net>
2023-02-28 10:58:50 -0700

    Submit form to /contracts/new using method=POST so that length is ok.  TODO: make recipient read from URL parameters to detect address, or new *and then* read POST variables if present.

--- simplestupidui/pages/index.tsx
+++ simplestupidui/pages/index.tsx
@@ -8,15 +8,22 @@ import { ethers } from "ethers";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
-
   const handleSubmit = async (event: FormEvent) => {
-    // Stop the form from submitting and refreshing the page.
-    event.preventDefault()
+    const form = event.target as HTMLFormElement
+
+    if(form.contractAddress.length == 0 || form.contractAbi.length==0){
+      // Stop the form from submitting and refreshing the page.
+      event.preventDefault();
+
+      //show error
+      alert("Please provide address and ABI");
+
+      return;
+    }
 
     console.log("invoke api")
 
     // // Cast the event target to an html form
-    // const form = event.target as HTMLFormElement
 
     // // Get data from the form.
     // const data = {
@@ -43,6 +50,307 @@ export default function Home() {
     
   }
 
+  const abiPlaceholder = `{
+    "_format": "hh-sol-artifact-1",
+    "contractName": "GregToken",
+    "sourceName": "contracts/GregToken.sol",
+    "abi": [
+      {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        ...etc`;
+
+  const gregTokenAbiAsString = `{
+    "_format": "hh-sol-artifact-1",
+    "contractName": "GregToken",
+    "sourceName": "contracts/GregToken.sol",
+    "abi": [
+      {
+        "inputs": [],
+        "stateMutability": "nonpayable",
+        "type": "constructor"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Approval",
+        "type": "event"
+      },
+      {
+        "anonymous": false,
+        "inputs": [
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "indexed": true,
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "indexed": false,
+            "internalType": "uint256",
+            "name": "value",
+            "type": "uint256"
+          }
+        ],
+        "name": "Transfer",
+        "type": "event"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "owner",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          }
+        ],
+        "name": "allowance",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "approve",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "account",
+            "type": "address"
+          }
+        ],
+        "name": "balanceOf",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "decimals",
+        "outputs": [
+          {
+            "internalType": "uint8",
+            "name": "",
+            "type": "uint8"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "subtractedValue",
+            "type": "uint256"
+          }
+        ],
+        "name": "decreaseAllowance",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "spender",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "addedValue",
+            "type": "uint256"
+          }
+        ],
+        "name": "increaseAllowance",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "name",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "symbol",
+        "outputs": [
+          {
+            "internalType": "string",
+            "name": "",
+            "type": "string"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [],
+        "name": "totalSupply",
+        "outputs": [
+          {
+            "internalType": "uint256",
+            "name": "",
+            "type": "uint256"
+          }
+        ],
+        "stateMutability": "view",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "transfer",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      },
+      {
+        "inputs": [
+          {
+            "internalType": "address",
+            "name": "from",
+            "type": "address"
+          },
+          {
+            "internalType": "address",
+            "name": "to",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "amount",
+            "type": "uint256"
+          }
+        ],
+        "name": "transferFrom",
+        "outputs": [
+          {
+            "internalType": "bool",
+            "name": "",
+            "type": "bool"
+          }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+      }
+    ],
+    "bytecode": "removed",
+    "deployedBytecode": "removed",
+    "linkReferences": {},
+    "deployedLinkReferences": {}
+  }`;
+
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
@@ -51,22 +359,22 @@ export default function Home() {
             <label>Host My Contract</label>
           </div>
           <div className='formSection'>
-            <form onSubmit={handleSubmit} className='solidityForm'>
+            <form onSubmit={handleSubmit} className='solidityForm' method='post' action='/contracts/new'>
               <div className='addressSection formRow'>
                 <div className='formLabel'>
-                  <label htmlFor="contractAddres">Address</label> 
+                  <label htmlFor="contractAddress">Contract Deployment Address</label> 
                 </div>
                 <div className='formInput'>
-                  <input type="text" id="contractAddres" name="contractAddres" className='contractAddress' required />   
-                </div>                                          
+                  <input type="text" id="contractAddress" name="contractAddres" className='contractAddress' required minLength="5" size="52" placeholder='0xa4e4745a1066ac0faebe4e005793b172c69cc9c4' />
+                </div>
               </div>
               <div className='codeSection formRow'>
                 <div className='formLabel'>
-                  <label htmlFor="contractCode">Smart Contract Code:</label>   
+                  <label htmlFor="contractAbi">Contract ABI:</label>   
                 </div>
                 <div className='formInput'>
-                  <textarea name="contractCode" id="contractCode" className='contractCode' rows={500} cols={200} required/>
-                </div>             
+                  <textarea name="contractAbi" id="contractAbi" className='contractAbi' rows={50} cols={750} required placeholder={abiPlaceholder} />
+                </div>
                 
               </div>
               <div className='submitSection formRow'>
@@ -76,7 +384,7 @@ export default function Home() {
                 <div className='formInput'>
                   <button type="submit" className='submit'>Host</button>
                 </div>
-              </div>                           
+              </div>
             </form>
           </div>
         </div>
